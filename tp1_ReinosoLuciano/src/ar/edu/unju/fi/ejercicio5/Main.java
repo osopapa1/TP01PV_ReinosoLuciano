ed9818d3903bd9ec6869cd15ba30f9d7e825d604
osopapa1 <166052671+osopapa1@users.noreply.github.com>
2024-04-15 18:47:56 -0300

    Ejercicio 5

--- tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio5/Main.java
+++ tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -1,1 +1,23 @@
+package ar.edu.unju.fi.ejercicio5;
 
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int num = 0;
+		
+		do {
+			System.out.println("Ingrese un numero: ");
+			num = sc.nextInt();
+		} while (num < 1 || num > 9);
+	
+		
+		for (int i = 0; i < 11; i++) {
+			System.out.println(+num+" x "+i+" = "+(num*i));			
+		}
+		
+	}
+
+}
