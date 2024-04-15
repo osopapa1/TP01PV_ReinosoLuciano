442ce283efeb9ef33451fa65a1aee587ef04a4e4
osopapa1 <166052671+osopapa1@users.noreply.github.com>
2024-04-15 18:45:39 -0300

    Ejercicio 2

--- tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -1,1 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
 
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argelia";
+		int personaEdad = 18;
+		Float alturaEdificio = (float) 130.5;
+		Float precioProducto = (float) 2350.5;
+		String numeroTel = "388 584 1834";
+		Float coseno = (float) 0.5;
+		
+		System.out.println("Nombre del Pais: "+nombrePais);
+		System.out.println("Edad de la persona: "+personaEdad);
+		System.out.println("Altura del edificio: "+alturaEdificio);
+		System.out.println("Precio del producto: "+precioProducto);
+		System.out.println("Número de teléfono: "+numeroTel);
+		System.out.println("Coseno del angulo 0.5: "+Math.cos(coseno));
+
+ 	}
+
+}
