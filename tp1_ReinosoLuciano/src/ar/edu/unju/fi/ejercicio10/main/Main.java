22e08342b133d77a5e456865daafc13734ce90db
osopapa1 <166052671+osopapa1@users.noreply.github.com>
2024-04-15 18:33:52 -0300

    Ejercicio 10

--- tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio10/main/Main.java
+++ tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio10/main/Main.java
@@ -1,10 +1,31 @@
 package ar.edu.unju.fi.ejercicio10.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio10.model.Pizza;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Pizza[] pizza = new Pizza[3];
+		Scanner sc = new Scanner(System.in);
+		int diametro;
+		boolean ingred_esp;
+		for(int i=0;i<3;i++) {
+			pizza[i] = new Pizza();
+			System.out.println("Tamaño de la Pizza?(20cm, 30cm o 40cm) ");
+				diametro = sc.nextInt();
+				sc.nextLine();
+			System.out.println("Ingredientes especiales?(true/false) ");
+				ingred_esp = sc.nextBoolean();
+			pizza[i].setDiametro(diametro);
+			pizza[i].setIngredEsp(ingred_esp);
+		}
+		
+		for(int i=0;i<3;i++) {
+			pizza[i].MostrarDatos();
+		}
+		sc.close();
 	}
 
 }
