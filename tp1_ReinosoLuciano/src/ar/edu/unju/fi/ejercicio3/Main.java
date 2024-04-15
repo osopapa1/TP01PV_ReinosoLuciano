442aaf0e7fa681383105c17866cd5cc00ca397f3
osopapa1 <166052671+osopapa1@users.noreply.github.com>
2024-04-15 18:46:15 -0300

    Ejercicio 3

--- tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio3/Main.java
+++ tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -1,1 +1,20 @@
+package ar.edu.unju.fi.ejercicio3;
 
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Ingrese un número: ");
+		int n1 = sc.nextInt();
+		if ( n1 % 2 == 0) {
+		System.out.println("El número es par. Su triple es: " +n1*3);
+		}
+		else {
+			System.out.println("El número es impar. Su doble es: "+n1*2);
+		}
+
+	}
+
+}
