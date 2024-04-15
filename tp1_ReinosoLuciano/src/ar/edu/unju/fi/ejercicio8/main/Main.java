1beacffebc13d0032236f8227bcd0395e2697dc9
osopapa1 <166052671+osopapa1@users.noreply.github.com>
2024-04-15 19:05:34 -0300

    Ejercicio 8

--- tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio8/main/Main.java
+++ tp1_ReinosoLuciano/src/ar/edu/unju/fi/ejercicio8/main/Main.java
@@ -7,14 +7,14 @@ import ar.edu.unju.fi.ejercicio8.model.CalculadoraEspecial;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingrese un valor: ");
-		int n = scanner.nextInt();
+		int n = sc.nextInt();
 		CalculadoraEspecial cal = new CalculadoraEspecial();
 		cal.setN(n);
 		System.out.println("\nResultado de la Sumatoria: "+cal.CalcularSumatoria());
 		System.out.println("\nResultado de la Productoria: "+cal.CalcularProductoria());
-		scanner.close();
+		sc.close();
 	}
 
 }
